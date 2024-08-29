7a11c2c9e49b971a87f5b0a1eab64f71b5ed24be
valer <valeriibadirov@mail.ru>
2024-08-29 15:32:45 +0300

    исправление багов при удалении проекта

--- resources/views/components/forms/inputs/multiple-files.blade.php
+++ resources/views/components/forms/inputs/multiple-files.blade.php
@@ -7,7 +7,7 @@
         <div class="flex relative items-center justify-center border rounded">
             <img src="{{ $value }}" class="max-w-full max-h-32">
             <a href="#" class="absolute top-1 right-1 text-orange">
-                <x-icons.x-circle class="h-5 w-5"/>
+                <x-icons.x-circle class="w-6 h-6 text-gray-800 dark:text-white"/>
             </a>
         </div>
     @endforeach
